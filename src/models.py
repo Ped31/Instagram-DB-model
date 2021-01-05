9283bf743a86fb8d41567675b15dbdb38ac4f0d9
Ped31 <pedrorivdom@gmail.com>
2021-01-05 20:12:22 +0000

    model finished

--- src/models.py
+++ src/models.py
@@ -8,23 +8,47 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    email = Column(String(120), nullable=False, unique=True)
+    following = Column(String(120)) #revisar esta relación
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    imageUrl = Column(String(250))
+    caption = Column(String(250))
+    comments = Column(String(250))
+    likes = Column(Integer)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)    
+
+class Comments(Base):
+    __tablename__ = 'comments'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    number_of_comments = Column(String(250))
+    who_comments_id = Column(Integer, nullable=False)
+    likes = Column(Integer)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
+class Relationship(Base):
+    __tablename__ = 'relationship'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    following_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
 
     def to_dict(self):
         return {}
